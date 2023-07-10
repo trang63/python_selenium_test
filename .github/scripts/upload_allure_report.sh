51cbf2ba4d9904e31d83d9abe73fea579a747d04
trang-than <trang.than@wizeline.com>
2023-07-10 23:10:39 +0700

    Update yaml

--- .github/scripts/upload_allure_report.sh
+++ .github/scripts/upload_allure_report.sh
@@ -3,4 +3,4 @@ git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 git add .
 git commit -m "Upload Allure Report"
-git push https://"$GIT_ACCESS_KEY"@github.com/"$GIT_NAME"/allure_report.git
+git push https://$GIT_ACCESS_KEY@github.com/$GIT_NAME/allure_report.git
